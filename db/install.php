d4e642d78aa240c284ee3be010aacab0230cd4fa
Rex Lorenzo <rex@oid.ucla.edu>
2017-09-15 15:32:47 -0700

    Added creation of anonymous user on install.
    * Also added deletion of user on uninstall.

--- db/install.php
+++ db/install.php
@@ -29,5 +29,25 @@ defined('MOODLE_INTERNAL') || die;
  * Set basic plugin settings.
  */
 function xmldb_local_anonymousposting_install() {
+    global $DB;
+
     set_config('enabled' , 1, 'local_anonymousposting');
+
+    // Install Anonymous user if not already installed.
+    if (!$anonroleid = $DB->record_exists('role', array('shortname' => 'anonstudent'))) {
+        $anonroleid = create_role(get_string('anonstudentrole', 'local_anonymousposting'),
+                'anonstudent', get_string('anonstudentroledesc', 'local_anonymousposting'));
+        set_role_contextlevels($anonroleid, array(CONTEXT_COURSE));
+        $context = context_system::instance();
+
+        // Add only necessary forum capabilities.
+        assign_capability('mod/forum:createattachment', CAP_ALLOW, $anonroleid, $context->id);
+        assign_capability('mod/forum:replypost', CAP_ALLOW, $anonroleid, $context->id);
+        assign_capability('mod/forum:startdiscussion', CAP_ALLOW, $anonroleid, $context->id);
+        assign_capability('mod/forum:viewdiscussion', CAP_ALLOW, $anonroleid, $context->id);
+    }
+
+    // Set Default course role and activity role to anonstudent.
+    set_config('defaultcourserole' , $anonroleid, 'local_anonymousposting');
+    set_config('defaultactivityrole' , $anonroleid, 'local_anonymousposting');
 }
